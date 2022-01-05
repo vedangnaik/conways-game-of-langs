7b5caa4c23b7c608a90af16e9c4124eb95e17293
Vedang Naik <vedang.a.naik@gmail.com>
2022-01-05 01:58:08 -0500

    Fixed package path

--- java/src/main/java/game_of_life/app/Main.java
+++
@@ -1,9 +0,0 @@
-package game_of_life.main;
-
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ java/src/main/java/game_of_life/main/Main.java
@@ -0,0 +1,11 @@
+package game_of_life.main;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for (String s: args) {
+            System.out.println(s);
+        }
+    }
+}
