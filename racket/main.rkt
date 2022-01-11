2ccc0f135dc3f1c888df0f708066a9478b8fb65a
Vedang Naik <vedang.a.naik@gmail.com>
2022-01-11 03:50:23 -0500

    Added struct for Board and PBM output

--- racket/main.rkt
+++ racket/main.rkt
@@ -1,5 +1,29 @@
 #lang racket
 
+(struct Board (b size))
+
+(define (saveBoardAsPBMP1 board filename)
+  (call-with-output-file filename #:exists 'replace
+    (lambda (out)
+      (define size (Board-size board))
+      (define b (Board-b board))
+      (fprintf out "P1\n~a ~a\n" size size)
+      (for ([x (in-range size)])
+        (for ([y (in-range size)])
+          (fprintf out "~a " 
+            (if (vector-ref (vector-ref b x) y)
+              "1"
+              "0"
+            )
+          )
+        )
+        (fprintf out "\n")
+      )
+    )
+  )
+  (void)
+)
+
 (define (main)
   ; TODO: Read in size and number of simulation turns
   (define size 40)
@@ -7,22 +31,25 @@
 
   ; Set up board
   (define board
-    (build-vector
-      size
-      (lambda (i)
-        (build-vector
-          size
-          (lambda (i)
-            (vector #f)
+    (Board
+      (build-vector
+        size
+        (lambda (i)
+          (build-vector
+            size
+            (lambda (i)
+              #f
+            )
           )
         )
       )
+      size
     )
   )
-  
+
   ; Parse input file
   (define coords_str (string-split (port->string (open-input-file inital-state-file))))
-  (define coords (map 
+  (define coords (map
     (lambda (c)
       (define i (string->number c))
       (cond
@@ -43,9 +70,11 @@
   (for ([i (in-range 0 (length coords) 2)])
     (define x (list-ref coords i))
     (define y (list-ref coords (+ i 1)))
-    (vector-set! (vector-ref board x) y #t)
+    (vector-set! (vector-ref (Board-b board) x) y #t)
   )
-  
+
+  (saveBoardAsPBMP1 board "test.pbm")
+
   0
 )
 
