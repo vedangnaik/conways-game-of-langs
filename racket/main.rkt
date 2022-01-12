40d59ef95cd7c64a1db6361b2efb8d2805814bc3
Vedang Naik <vedang.a.naik@gmail.com>
2022-01-11 22:36:09 -0500

    Moved to Racket classes intead of structs

--- racket/main.rkt
+++ racket/main.rkt
@@ -1,18 +1,47 @@
 #lang racket
 
-(struct Board (b size))
+(define Board% (class object%
+  (init size)
+
+  (define s size)
+  (define b 
+    (build-vector
+      s
+      (lambda (i)
+        (build-vector
+          s
+          (lambda (i)
+            #f
+          )
+        )
+      )
+    )
+  )
+  (super-new)
+
+  (define/public (is-set row col)
+    (vector-ref (vector-ref b row) col)
+  )
+
+  (define/public (set row col)
+    (vector-set! (vector-ref b row) col #t)
+  )
+
+  (define/public (get-size)
+    s
+  )
+))
 
 (define (saveBoardAsPBMP1 board filename)
   (call-with-output-file filename #:exists 'replace
     (lambda (out)
-      (define size (Board-size board))
-      (define b (Board-b board))
+      (define size (send board get-size))
       
       (fprintf out "P1\n~a ~a\n" size size)
-      (for ([x (in-range size)])
-        (for ([y (in-range size)])
+      (for ([row (in-range size)])
+        (for ([col (in-range size)])
           (fprintf out "~a " 
-            (if (vector-ref (vector-ref b x) y)
+            (if (send board is-set row col)
               "1"
               "0"
             )
@@ -25,63 +54,39 @@
   (void)
 )
 
-(define (boardIsSet board x y)
-  (vector-ref (vector-ref (Board-b board) x) y)
-)
-
-(define (setBoard board x y)
-  (vector-set! (vector-ref (Board-b board) x) y #t)
-)
-
 (define (getNumNeighbors board row col)
-  (define size (Board-size board))
-  
+  (define size (send board get-size))
+
   (define count 0)
   (define row_lower (modulo (- row 1) size))
   (define row_upper (modulo (+ row 1) size))
   (define col_lower (modulo (- col 1) size))
   (define col_upper (modulo (+ col 1) size))
 
-  (set! count (+ count (if (boardIsSet board row_lower col_lower) 1 0 )))
-  (set! count (+ count (if (boardIsSet board row_lower col      ) 1 0 )))
-  (set! count (+ count (if (boardIsSet board row_lower col_upper) 1 0 )))
-  (set! count (+ count (if (boardIsSet board row       col_lower) 1 0 )))
-  (set! count (+ count (if (boardIsSet board row       col_upper) 1 0 )))
-  (set! count (+ count (if (boardIsSet board row_upper col_lower) 1 0 )))
-  (set! count (+ count (if (boardIsSet board row_upper col      ) 1 0 )))
-  (set! count (+ count (if (boardIsSet board row_upper col_upper) 1 0 )))
+  (set! count (+ count (if (send board is-set row_lower col_lower) 1 0 )))
+  (set! count (+ count (if (send board is-set row_lower col      ) 1 0 )))
+  (set! count (+ count (if (send board is-set row_lower col_upper) 1 0 )))
+  (set! count (+ count (if (send board is-set row       col_lower) 1 0 )))
+  (set! count (+ count (if (send board is-set row       col_upper) 1 0 )))
+  (set! count (+ count (if (send board is-set row_upper col_lower) 1 0 )))
+  (set! count (+ count (if (send board is-set row_upper col      ) 1 0 )))
+  (set! count (+ count (if (send board is-set row_upper col_upper) 1 0 )))
 
   count
 )
 
-(define (initBoard size)
-  (Board
-    (build-vector
-      size
-      (lambda (i)
-        (build-vector
-          size
-          (lambda (i)
-            #f
-          )
-        )
-      )
-    )
-    size
-  )
-)
-
 (define (main)
   ; TODO: Read in size and number of simulation turns
-  (define size 40)
-  (define num-timesteps 100)
-  (define inital-state-file "gosper_glider_gun.txt")
+  (define user-size 40)
+  (define user-num-timesteps 100)
+  (define user-inital-state-file "gosper_glider_gun.txt")
 
   ; Set up board
-  (define board (initBoard size))
+  (define board (new Board% [size user-size]))
+  (define size (send board get-size))
 
   ; Parse input file
-  (define coords_str (string-split (port->string (open-input-file inital-state-file))))
+  (define coords_str (string-split (port->string (open-input-file user-inital-state-file))))
   (define coords (map
     (lambda (c)
       (define i (string->number c))
@@ -95,7 +100,7 @@
   ))
   ; Check for odd number of coordinates
   (if (not (equal? (modulo (length coords) 2) 0))
-    (raise (printf "There are an odd number of coordinates in ~a." inital-state-file))
+    (raise (printf "There are an odd number of coordinates in ~a." user-inital-state-file))
     (void)
   )
 
@@ -103,20 +108,20 @@
   (for ([i (in-range 0 (length coords) 2)])
     (define x (list-ref coords i))
     (define y (list-ref coords (+ i 1)))
-    (setBoard board x y)
+    (send board set x y)
   )
 
   ; Main simulation loop
-  (for ([timestep (in-range num-timesteps)])
+  (for ([timestep (in-range user-num-timesteps)])
     (saveBoardAsPBMP1 board (format "~a.pbm" timestep))
 
-    (define nextBoard (initBoard size))
+    (define nextBoard (new Board% [size user-size]))
     (for ([row (in-range size)])
       (for ([col (in-range size)])
         (define num-neighbors (getNumNeighbors board row col))
-        (if (boardIsSet board row col)
-          (if (<= 2 num-neighbors 3)   (setBoard nextBoard row col) void)
-          (if (equal? num-neighbors 3) (setBoard nextBoard row col) void)
+        (if (send board is-set row col)
+          (if (<= 2 num-neighbors 3)   (send nextBoard set row col) void)
+          (if (equal? num-neighbors 3) (send nextBoard set row col) void)
         )
       )
     )
